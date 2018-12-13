eb9f5f6d78b92d88bc31e67ecb12090db585d52e
mac <mac@macdeMacBook-Pro.local>
2018-12-13 15:27:11 +0800

    newxibvc

---
+++ WZZDeveloperCalculator/WZZDeveloperCalculator/WZZXibVC.h
@@ -0,0 +1,17 @@
+//
+//  WZZXibVC.h
+//  WZZDeveloperCalculator
+//
+//  Created by mac on 2018/12/13.
+//  Copyright © 2018 wzz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WZZXibVC : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ WZZDeveloperCalculator/WZZDeveloperCalculator/WZZXibVC.m
@@ -0,0 +1,32 @@
+//
+//  WZZXibVC.m
+//  WZZDeveloperCalculator
+//
+//  Created by mac on 2018/12/13.
+//  Copyright © 2018 wzz. All rights reserved.
+//
+
+#import "WZZXibVC.h"
+
+@interface WZZXibVC ()
+
+@end
+
+@implementation WZZXibVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
